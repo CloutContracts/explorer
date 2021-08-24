b86126c81456c79263b18c86275b7365086e8b0a
Andrew Kamal <kamalandrew55@gmail.com>
2021-08-24 19:24:12 +0000

    Made changes

--- js/bnbpeg.js
+++ js/bnbpeg.js
@@ -1,7 +1,9 @@
 // bnbpeg click event
 $('#bnbpeg-token').click(function () {
-    $('#bnbpeg-card').html("");
-    let element_id = '#bnbpeg-card';
+    bnbTab = 'token';
+
+    $('#bnb-table tbody').html("");
+    let element_id = '#bnb-table';
     // let token = "TH6XW2FQY1I8CDFWVD6YD9XCRYHYTUQ9BD";
     let api_url = "https://api.bscscan.com/api?module=account&action=txlist&address=0x3e3B357061103DC040759aC7DceEaba9901043aD&startblock=1&endblock=99999999&sort=asc&apikey=TH6XW2FQY1I8CDFWVD6YD9XCRYHYTUQ9BD"
 
@@ -11,8 +13,10 @@ $('#bnbpeg-token').click(function () {
 
 
 $('#bnbpeg-rollup').click(function () {
-    $('#bnbpeg-card').html("");
-    let element_id = '#bnbpeg-card';
+    bnbTab = 'rollup';
+
+    $('#bnb-table tbody').html("");
+    let element_id = '#bnb-table';
     // let token = "TH6XW2FQY1I8CDFWVD6YD9XCRYHYTUQ9BD"
     let api_url = "https://api.bscscan.com/api?module=account&action=txlist&address=0xABa46894aCaB62A47Ff28c0a69e6333B80425dA5&startblock=1&endblock=99999999&sort=asc&apikey=TH6XW2FQY1I8CDFWVD6YD9XCRYHYTUQ9BD"
 
@@ -27,64 +31,56 @@ function bnbPegToken(api_url, element_id) {
             if (data.status) {
                 var html = ''
                 $.each(data.result, function (index, value) {
-                    html += '<div class="card no-border mb-3 bg-dark1">' +
-                        '<div class="card-body bg-dark1 mb-3">' +
-                        '<div class="row mb-2">' +
-                        '<div class="col-sm-2 col-md-2">' +
-                        '<div class="row">' +
-                        '<div class="col-sm-12 col-md-12">' +
-                        '<h6 class="text-white text-center">#' + value.blockNumber + '</h6>' +
-                        '</div>' +
-                        '</div>' +
-                        '<div class="row">' +
-                        '<div class="col-sm-12 col-md-12">' +
-                        '<h6 class="text-purple text-center">Block</h6>' +
-                        '</div>' +
-                        '</div>' +
-                        '</div>' +
-                        '<div class="col-sm-6 col-md-6">' +
-                        '<div class="row">' +
-                        '<div class="col-sm-4 col-md-4">' +
-                        '<p class="text-light-p">3 Transactions </p>' +
-                        '</div>' +
-                        '<div class="col-sm-4 col-md-4">' +
-                        '<p class="text-light-p"> 1,840 bytes </p>' +
-                        '</div>' +
-                        '<div class="col-sm-4 col-md-4">' +
-                        '<p class="text-light-p">35 seconds ago </p>' +
-                        '</div>' +
-                        '</div>' +
-                        '<div class="row">' +
-                        '<div class="col-sm-2 col-md-2">' +
-                        '<p class="text-light-p text-left">Miner</p>' +
-                        '</div>' +
-                        '<div class="col-sm-10 col-md-10">' +
-                        '<h6 class="text-purple text-center hash">' + value.hash + '</h6>' +
-                        '</div>' +
-                        '</div>' +
-                        '<div class="row">' +
-                        '<div class="col-sm-4 col-md-4">' +
-                        '<p class="text-light-p text-left">Reward</p>' +
-                        '</div>' +
-                        '<div class="col-sm-4 col-md-4">' +
-                        '<p class="text-light-p text-center">3.2 BNB</p>' +
-                        '</div>' +
-                        '</div>' +
-                        '</div>' +
-                        '<div class="col-sm-4 col-md-4">' +
-                        '<p class="text-light-p text-right">' + value.gasUsed + ' Gas Used</p>' +
-                        '</div>' +
-                        '</div>' +
-                        '</div>' +
-                        '</div>';
-
-                    if (!$.inArray(tokens,value)) {
-                        tokens.push(value);
+                    const date = new Date(value.timeStamp * 1000);
+                    if (searchToken != '') {
+                        if (searchToken == value.hash.toString()) {
+                            html += '<tr>'+
+                                '<td class="text-left">'
+                                    +value.blockNumber+
+                                '</td>'+
+                                '<td class="text-left">'
+                                    +value.hash.toString()+
+                                '</td>'+
+                                '<td class="text-left">'
+                                    +value.gasUsed+
+                                '</td>'+
+                                '<td class="text-left">'+
+                                    '3.5 ETH'+
+                                '</td>'+
+                                '<td class="text-left">'
+                                    +date.toLocaleTimeString()+
+                                '</td>'+
+                            '</tr>';
+                        }
+                    } else {
+                        html += '<tr>'+
+                                    '<td class="text-left">'
+                                        +value.blockNumber+
+                                    '</td>'+
+                                    '<td class="text-left">'
+                                        +value.hash.toString()+
+                                    '</td>'+
+                                    '<td class="text-left">'
+                                        +value.gasUsed+
+                                    '</td>'+
+                                    '<td class="text-left">'+
+                                        '3.5 ETH'+
+                                    '</td>'+
+                                    '<td class="text-left">'
+                                        +date.toLocaleTimeString()+
+                                    '</td>'+
+                                '</tr>';
                     }
 
                 });
 
-                $(element_id).append(html);
+                if (data.result.length == 0) {
+                    html = '<div style="color: #8A8DBA !important;">No records found!</div>'
+                } else if (html == '') {
+                    html = '<div style="color: #8A8DBA !important;">No records found!</div>'
+                }
+
+                $('#bnb-table').append(html);
                 
             }
         });
@@ -97,60 +93,56 @@ function bnbPegRollup(api_url, element_id) {
             if (data.status) {
                 var html = ''
                 $.each(data.result, function (index, value) {
-                    html += '<div class="card no-border mb-3 bg-dark1">' +
-                        '<div class="card-body bg-dark1 mb-3">' +
-                        '<div class="row mb-2">' +
-                        '<div class="col-sm-2 col-md-2">' +
-                        '<div class="row">' +
-                        '<div class="col-sm-12 col-md-12">' +
-                        '<h6 class="text-white text-center">#' + value.blockNumber + '</h6>' +
-                        '</div>' +
-                        '</div>' +
-                        '<div class="row">' +
-                        '<div class="col-sm-12 col-md-12">' +
-                        '<h6 class="text-purple text-center">Block</h6>' +
-                        '</div>' +
-                        '</div>' +
-                        '</div>' +
-                        '<div class="col-sm-6 col-md-6">' +
-                        '<div class="row">' +
-                        '<div class="col-sm-4 col-md-4">' +
-                        '<p class="text-light-p">3 Transactions </p>' +
-                        '</div>' +
-                        '<div class="col-sm-4 col-md-4">' +
-                        '<p class="text-light-p"> 1,840 bytes </p>' +
-                        '</div>' +
-                        '<div class="col-sm-4 col-md-4">' +
-                        '<p class="text-light-p">35 seconds ago </p>' +
-                        '</div>' +
-                        '</div>' +
-                        '<div class="row">' +
-                        '<div class="col-sm-2 col-md-2">' +
-                        '<p class="text-light-p text-left">Miner</p>' +
-                        '</div>' +
-                        '<div class="col-sm-10 col-md-10">' +
-                        '<h6 class="text-purple text-center hash">' + value.hash + '</h6>' +
-                        '</div>' +
-                        '</div>' +
-                        '<div class="row">' +
-                        '<div class="col-sm-4 col-md-4">' +
-                        '<p class="text-light-p text-left">Reward</p>' +
-                        '</div>' +
-                        '<div class="col-sm-4 col-md-4">' +
-                        '<p class="text-light-p text-center">3.2 BNB</p>' +
-                        '</div>' +
-                        '</div>' +
-                        '</div>' +
-                        '<div class="col-sm-4 col-md-4">' +
-                        '<p class="text-light-p text-right">' + value.gasUsed + ' Gas Used</p>' +
-                        '</div>' +
-                        '</div>' +
-                        '</div>' +
-                        '</div>';
+                    const date = new Date(value.timeStamp * 1000);
+                    if (searchToken != '') {
+                        if (searchToken == value.hash.toString()) {
+                            html += '<tr>'+
+                                    '<td class="text-left">'+
+                                        +value.blockNumber+
+                                    '</td>'+
+                                    '<td class="text-left">'
+                                        +value.hash.toString()+
+                                    '</td>'+
+                                    '<td class="text-left">'+
+                                        +value.gasUsed+
+                                    '</td>'+
+                                    '<td class="text-left">'+
+                                        '3.5 BNB'+
+                                    '</td>'+
+                                    '<td class="text-left">'
+                                        +date.toLocaleTimeString()+
+                                    '</td>'+
+                            '</tr>';
+                        }
+                    } else {
+                        html += '<tr>'+
+                                    '<td class="text-left">'+
+                                        +value.blockNumber+
+                                    '</td>'+
+                                    '<td class="text-left">'
+                                        +value.hash.toString()+
+                                    '</td>'+
+                                    '<td class="text-left">'+
+                                        +value.gasUsed+
+                                    '</td>'+
+                                    '<td class="text-left">'+
+                                        '3.5 BNB'+
+                                    '</td>'+
+                                    '<td class="text-left">'
+                                        +date.toLocaleTimeString()+
+                                    '</td>'+
+                            '</tr>';
+                    }
 
                 });
 
-                $(element_id).append(html);
+                if (data.result.length == 0) {
+                    html = '<div style="color: #8A8DBA !important;">No records found!</div>'
+                } else if (html == '') {
+                    html = '<div style="color: #8A8DBA !important;">No records found!</div>'
+                }
+
+                $('#bnb-table tbody').append(html);
             }
         });
 }
